8bd0eeec2d1bec0845dbf1f73e848f30913ca51c
Petr <lithx@yandex.com>
2017-06-03 19:47:00 +0300

    Moved default bachground URL to constant

--- resources/assets/js/_variables.js
+++ resources/assets/js/_variables.js
@@ -9,3 +9,8 @@ const COLUMN_COUNT = 12;
  * Available column values.
  */
 const AVAILABLE_COLUMNS = [1, 2, 3, 4, 6, 12];
+
+/**
+ * Default wallpaper URL
+ */
+const DEFAULT_WALLPAPER_URL = 'https://i.imgur.com/Lsnc2yC.jpg';
