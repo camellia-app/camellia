031bafb0d79b6178a8bc7b54dfb2cc81362a770d
Petr Flaks <petr@flaks.dev>
2020-03-19 16:38:39 +0300

    Remove IndeterminateIndicator

--- src/components/IndeterminateIndicator/IndeterminateIndicator.tsx
+++
@@ -1,8 +0,0 @@
-import { h } from 'preact';
-import * as s from './IndeterminateIndicator.css';
-
-export default () => (
-  <div className={s.track}>
-    <progress className={s.progress} />
-  </div>
-);
