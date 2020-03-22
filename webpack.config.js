180b023d04d3d98466131608d96b732ac5ff241e
Petr Flaks <petr@flaks.dev>
2020-03-22 13:35:06 +0300

    Add support for multiple environments

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,26 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 const package = require('./package.json');
 
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = 'local';
+
+  console.warn('Environment variable NODE_ENV is not defined, using "local" as default value');
+}
+
+if (process.env.BUILD_NUMBER === undefined) {
+  process.env.BUILD_NUMBER = 0;
+
+  console.warn('Environment variable BUILD_NUMBER is not defined, using "0" as default value');
+}
+
 module.exports = () => {
-  const config = {
-    context: `${__dirname}/src`,
-    devtool: 'source-map',
+  const commonConfig = {
+    context: path.join(__dirname, 'src'),
     entry: {
       background: './background.js',
-      newtab: './newtab',
+      newtab: './newtab.tsx',
     },
+    mode: 'production',
     module: {
       rules: [
         {
@@ -46,7 +58,6 @@ module.exports = () => {
         },
       ],
     },
-    name: 'client',
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, 'dist'),
@@ -54,7 +65,6 @@ module.exports = () => {
     plugins: [
       new CleanWebpackPlugin(),
       new Dotenv(),
-      new LiveReloadPlugin(),
       new CopyWebpackPlugin([
         {
           from: './manifest.json',
@@ -63,29 +73,38 @@ module.exports = () => {
 
             manifest.author = package.author.name;
             manifest.homepage_url = package.homepage;
-            manifest.version = package.version;
-            manifest.version_name = `${package.version} (DP)`;
-            manifest.content_security_policy = `${manifest.content_security_policy} script-src-elem 'self' http://localhost:35729;`;
+            manifest.version = `${package.version}.${process.env.BUILD_NUMBER}`;
+            manifest.version_name = `${package.version} build ${process.env.BUILD_NUMBER}`;
 
-            return JSON.stringify(manifest, null, 2);
+            switch (process.env.NODE_ENV) {
+              case 'local':
+                manifest.name = `${manifest.name} (dev)`;
+                manifest.version_name = `${manifest.version_name} (dev)`;
+                manifest.content_security_policy = `${manifest.content_security_policy} script-src-elem 'self' http://localhost:35729;`;
+
+                break;
+
+              case 'canary':
+                manifest.name = `${manifest.name} (canary)`;
+                manifest.version_name = `${manifest.version_name} (canary)`;
+
+                break;
+
+              default:
+                break;
+            }
+
+            return JSON.stringify(manifest, null, process.env.NODE_ENV === 'local' ? 2 : 0);
           },
         },
         { from: './logo.png' },
       ], {
         copyUnmodified: true,
       }),
-      new webpack.WatchIgnorePlugin([
-        /css\.d\.ts$/,
-      ]),
       new HtmlWebpackPlugin({
         chunks: ['newtab'],
         filename: 'newtab.html',
-        hash: true,
         inject: 'body',
-        liveReload: {
-          enabled: true,
-          scriptUrl: 'http://localhost:35729/livereload.js',
-        },
         template: './newtab.ejs',
       }),
     ],
@@ -93,12 +112,37 @@ module.exports = () => {
       extensions: ['.ts', '.tsx', '.js', 'jsx'],
     },
     target: 'web',
-    watchOptions: {
-      aggregateTimeout: 300,
-      ignored: /node_modules/,
-      poll: 1000,
-    },
   };
 
-  return config;
+  const env = process.env.NODE_ENV;
+
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      break;
+
+    case 'canary':
+      break;
+
+    case 'local':
+      commonConfig.mode = 'development';
+      commonConfig.devtool = 'source-map';
+
+      commonConfig.watchOptions = {
+        aggregateTimeout: 300,
+        ignored: /node_modules/,
+        poll: 1000,
+      };
+
+      commonConfig.plugins.push(new LiveReloadPlugin());
+      commonConfig.plugins.push(new webpack.WatchIgnorePlugin([
+        /css\.d\.ts$/,
+      ]));
+
+      break;
+
+    default:
+      throw Error('Unknown environment name');
+  }
+
+  return commonConfig;
 };
