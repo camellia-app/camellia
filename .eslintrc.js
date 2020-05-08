2a7c32e0a9dc29f9c1cdc47ffcd788a063e94a98
Petr Flaks <petr@flaks.dev>
2020-05-08 16:22:03 +0300

    Add React Hooks to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,15 @@ module.exports = {
   plugins: [
     '@typescript-eslint',
     'sort-keys-fix',
+    'react-hooks'
   ],
   root: true,
   rules: {
     "max-len": 0,
     "newline-before-return": ["error"],
     "react/destructuring-assignment": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
     "sort-keys": ["error", "asc", {"caseSensitive": true, "natural": false, "minKeys": 2}],
     "sort-keys-fix/sort-keys-fix": "warn",
   },
