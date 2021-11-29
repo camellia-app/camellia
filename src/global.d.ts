a60590d6514de2f2abf465aabcf94962c1b00edd
Petr <petr@flaks.dev>
2021-11-29 23:19:50 +0300

    refactor: remove global type definition for CSS modules

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,3 @@
-declare module '*.module.css' {
-  const classes: { readonly [key: string]: string };
-
-  /// eslint-disable-next-line import/no-default-export
-  export default classes;
-}
-
 declare module '*.svg' {
   const content: string;
   export default content;
