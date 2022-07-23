10e816fdd2e056e3322019888e0bd48c77ff417f
Petr Flaks <petr@flaks.dev>
2022-07-24 00:03:19 +0300

    chore: add `appVersion` and `isDevelopment` configs

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,13 @@ assertEnvironmentVariable(
   'Environment variable UNSPLASH_BRIDGE_BASE_HOST should be defined',
 );
 
+assertEnvironmentVariable(process.env['APP_VERSION'], 'Environment variable APP_VERSION should be defined');
+
+assertEnvironmentVariable(process.env['NODE_ENV'], 'Environment variable NODE_ENV should be defined');
+
 export const config = {
+  appVersion: process.env['APP_VERSION'],
+  isDevelopment: process.env['NODE_ENV'] !== 'production',
   unsplash: {
     bridge: {
       baseHost: new URL(process.env['UNSPLASH_BRIDGE_BASE_HOST']),
