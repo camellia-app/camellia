9ff14c76359e3b1ca160ef388a12a865d763c234
Petr Flaks <petr@flaks.dev>
2022-11-20 19:02:04 +0300

    chore: remove Storybook

--- .storybook/ProviderWrapper.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from 'react';
-import { Provider } from 'react-redux';
-import { store } from '../src/store';
-
-export const withProvider = (story: () => ReactNode): ReactNode => <Provider store={store}>{story()}</Provider>;
--- .storybook/preview.js
+++
@@ -1,7 +0,0 @@
-import '../src/reset.css';
-import '../src/variables.css';
-import '../src/commons.module.css';
-import { addDecorator } from '@storybook/react';
-import { withProvider } from './ProviderWrapper';
-
-addDecorator(withProvider);
