70d5f1a2459b2174bbab478385ba0ae7e6bddf94
Petr Flaks <petr@flaks.dev>
2022-11-20 13:34:47 +0300

    feat: show message when there are no bookmarks in a browser

--- src/components/BookmarkWorkspace/BookmarkWorkspace.module.css.d.ts
+++ src/components/BookmarkWorkspace/BookmarkWorkspace.module.css.d.ts
@@ -1,2 +1,5 @@
 export const bookmarkWorkspace: string;
 export const bookmarkWorkspaceLoading: string;
+export const bookmarkWorkspaceNoBookmarks: string;
+export const bookmarkWorkspaceNoBookmarksMessage: string;
+export const bookmarkWorkspaceNoBookmarksActions: string;
--- src/components/BookmarkWorkspace/BookmarkWorkspace.tsx
+++ src/components/BookmarkWorkspace/BookmarkWorkspace.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import type { FC } from 'react';
 import { useSelector } from 'react-redux';
+import { getSupportedRuntimeFeatures } from '../../api/applicationRuntime/features';
 import type { Bookmark } from '../../api/bookmark/common';
 import { useBookmarksBarChildren, useOtherBookmarksChildren } from '../../api/bookmark/hook';
 import { t } from '../../api/i18n/translate';
@@ -8,7 +9,15 @@ import type { RootState } from '../../store';
 import type { BookmarkSearchState } from '../../store/slice/bookmarkSearchSlice';
 import { BookmarkCategory } from '../BookmarkCategory/BookmarkCategory';
 import { BookmarkSearch } from '../BookmarkSearch/BookmarkSearch';
-import { bookmarkWorkspace, bookmarkWorkspaceLoading } from './BookmarkWorkspace.module.css';
+import { BookmarkManager } from '../BottomToolbar/ToolbarItem/BookmarkManager';
+import { ChipList } from '../ChipList/ChipList';
+import {
+  bookmarkWorkspace,
+  bookmarkWorkspaceLoading,
+  bookmarkWorkspaceNoBookmarks,
+  bookmarkWorkspaceNoBookmarksActions,
+  bookmarkWorkspaceNoBookmarksMessage,
+} from './BookmarkWorkspace.module.css';
 
 export const BookmarkWorkspace: FC = () => {
   const bookmarkSearchState = useSelector<RootState, BookmarkSearchState>((state) => state.bookmarkSearch);
@@ -16,8 +25,10 @@ export const BookmarkWorkspace: FC = () => {
   const [bookmarksBarChildren] = useBookmarksBarChildren();
   const [otherBookmarksChildren] = useOtherBookmarksChildren();
 
+  const isLoading = bookmarksBarChildren === undefined || otherBookmarksChildren === undefined;
+
   const mainClasses = classNames(bookmarkWorkspace, {
-    [bookmarkWorkspaceLoading]: bookmarksBarChildren === undefined || otherBookmarksChildren === undefined,
+    [bookmarkWorkspaceLoading]: isLoading,
   });
 
   const bookmarkCategories: Array<{ bookmarks: Array<Bookmark>; title: string }> = [];
@@ -43,14 +54,38 @@ export const BookmarkWorkspace: FC = () => {
     }
   }
 
+  const noBookmarksActionChips = [];
+
+  if (getSupportedRuntimeFeatures().bookmarkManagerPage) {
+    noBookmarksActionChips.push(<BookmarkManager />);
+  }
+
   return (
     <main className={mainClasses}>
       <BookmarkSearch />
-      {bookmarkCategories.map((bookmarkCategory, index) => {
-        return (
-          <BookmarkCategory bookmarks={bookmarkCategory.bookmarks} categoryTitle={bookmarkCategory.title} key={index} />
-        );
-      })}
+
+      {!isLoading && bookmarkCategories.length === 0 ? (
+        <div className={bookmarkWorkspaceNoBookmarks}>
+          <div className={bookmarkWorkspaceNoBookmarksMessage}>{t('bookmarkBrowser_noBookmarks_label')}</div>
+
+          {noBookmarksActionChips.length > 0 ? (
+            <div className={bookmarkWorkspaceNoBookmarksActions}>
+              <ChipList chips={noBookmarksActionChips} type={'inline'} />
+            </div>
+          ) : undefined}
+        </div>
+      ) : undefined}
+
+      {bookmarkCategories.length > 0 &&
+        bookmarkCategories.map((bookmarkCategory, index) => {
+          return (
+            <BookmarkCategory
+              bookmarks={bookmarkCategory.bookmarks}
+              categoryTitle={bookmarkCategory.title}
+              key={index}
+            />
+          );
+        })}
     </main>
   );
 };
